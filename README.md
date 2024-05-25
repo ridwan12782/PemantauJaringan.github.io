b5056f67de14db68547c85c07ebc0ae453381dc0
Ridwan <59315983+ridwan12782@users.noreply.github.com>
2024-05-25 07:24:53 +0700

    instalasi mikhmon

---
+++ README.md
@@ -0,0 +1,4 @@
+"# PemantauJaringanV3.5.githib.io" 
+# PemantauJaringan.github.io
+# PemantauJaringan.github.io
+# iwongwifi.net.github.io
---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['PemantauJaringan'] = array ('1'=>'PemantauJaringan<|<Pemantau','PemantauJaringan>|>gpKkoZ+ZmZ8=');
+$data['Iwonkswifi'] = array ('1'=>'Iwonkswifi!10.10.10.1','Iwonkswifi@|@admin','Iwonkswifi#|#gYiBhnhma2Zi','Iwonkswifi%Iwonks Wi-Fi','Iwonkswifi^iwongwifi.net','Iwonkswifi&Rp','Iwonkswifi*','Iwonkswifi(','Iwonkswifi)','Iwonkswifi=disable','Iwonkswifi@!@enable','Iwonkswifi#!#');
